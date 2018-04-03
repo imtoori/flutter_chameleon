4070982757db2c634ac91c746dfabe100a4184d9
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2018-04-03 13:18:59 +0200

    hotfix - export button and switch

--- lib/flutter_chameleon.dart
+++ lib/flutter_chameleon.dart
@@ -5,3 +5,5 @@ export 'chameleon_alert_dialog.dart';
 export 'chameleon_dialog.dart';
 export 'chameleon_activity_indicator.dart';
 export 'chameleon_slider.dart';
+export 'chameleon_button.dart';
+export 'chameleon_switch.dart';
